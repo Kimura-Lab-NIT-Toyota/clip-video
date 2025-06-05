b86e5687c85f35926463eb681a55a50b6ac4d039
Tamagosushio <mitachi.ts75@gmail.com>
2025-06-05 17:27:56 +0900

    [change] ffmpegの出力を削除

--- clip.py
+++ clip.py
@@ -10,27 +10,25 @@ if(os.path.exists(CONFIG_FILE)):
         config = json.load(f)
 else: raise Exception(f"not found {CONFIG_FILE}")
 
-# パスを全取得
-with open(config["video_path_file"], "r") as f:
-    paths = f.readlines()
+skip_path = set()
+
 # コアとなる時間を記したjsonを読み込む
 with open(config["time_file"], "r") as f:
     coretimes = json.load(f)
 # 未記入のパスがないかの確認
-for path in paths:
-    path = path.replace("\n", "").replace("\r", "")
-    if(coretimes[path]["start"] == -1 or coretimes[path]["end"] == -1):
-        print(path)
+for path in list(coretimes.keys()):
+    if(coretimes[path]["start"] == -1 or coretimes[path]["end"] == -1 or coretimes[path]["start"] > coretimes[path]["end"]):
+        print(f"Skip {path}")
+        skip_path.add(path)
 
 if(not os.path.exists(config["clipped_directory"])):
     os.mkdir(config["clipped_directory"])
-for path in paths:
+for path in list(coretimes.keys()):
     # 改行文字を消去
     path = path.replace("\n", "").replace("\r", "")
+    if(path in skip_path): continue
     # 動画ファイルのフレームレートを取得
     cap = cv2.VideoCapture(path)
-    print(cap.get(cv2.CAP_PROP_FPS))
-    print(coretimes[path])
     # 保存先となるディレクトリ、なければ作成
     dir_path = f"{config['clipped_directory']}/{path.split('/')[-2]}"
     if (not os.path.isdir(dir_path)):
@@ -39,8 +37,8 @@ for path in paths:
     stream = ffmpeg.input(
         path,
         ss=coretimes[path]["start"]/cap.get(cv2.CAP_PROP_FPS),
-        t =(coretimes[path]["end"] - coretimes[path]["start"]) / cap.get(cv2.CAP_PROP_FPS)
+        t =(coretimes[path]["end"] - coretimes[path]["start"]) / cap.get(cv2.CAP_PROP_FPS),
     ).output(f"{dir_path}/{path.split('/')[-1]}")
     # 保存
-    ffmpeg.run(stream)
+    ffmpeg.run(stream, quiet=True)
 
